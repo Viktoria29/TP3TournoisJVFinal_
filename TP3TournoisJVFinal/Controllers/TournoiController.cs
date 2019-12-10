fa777db0f44168fc314f328bb7c272f65a5cf681
Axel Chevarin <axel.chevarin1@uqac.ca>
2019-12-10 18:24:35 -0500

    Commint
    Final Change

--- TP3TournoisJVFinal/Controllers/TournoiController.cs
+++ TP3TournoisJVFinal/Controllers/TournoiController.cs
@@ -18,13 +18,11 @@ namespace TP3TournoisJVFinal.Controllers
             _context = context;
         }
 
-        // GET: Tournoi
         public async Task<IActionResult> Index()
         {
             return View(await _context.Tournois.ToListAsync());
         }
 
-        // GET: Tournoi/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -41,7 +39,6 @@ namespace TP3TournoisJVFinal.Controllers
 
             return View(tournoi);
         }
-        // GET: Search Tournoi By text
         public async Task<IActionResult> Recherche(string recherche)
         {
             return View("Index",await _context.Tournois.Where(t => t.Jeu.Contains(recherche) ||
@@ -76,8 +73,6 @@ namespace TP3TournoisJVFinal.Controllers
         }
 
         // POST: Tournoi/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("IdTournoi,Nom,Prix,Description,Date,Jeu,Theme")] Tournoi tournoi)
@@ -108,8 +103,7 @@ namespace TP3TournoisJVFinal.Controllers
         }
 
         // POST: Tournoi/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("IdTournoi,Nom,Prix,Description,Date,Jeu,Theme")] Tournoi tournoi)
